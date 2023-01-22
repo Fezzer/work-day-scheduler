52e26b2ffa79920ff28ec9fd0acfe5ddb503b070
Fezzer <chris.bowles@thescreamingsheep.net>
2023-01-22 20:09:48 +0000

    refactored getSettings() and added comments, also general refactoring

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,22 @@
 "use strict";
 
-
-// Application initialisation.
-$(() => {
+$(document).ready(() => {
   // Gets the settings for the application.
   function getSettings() {
+    // Properties in the config object can be edited to change the behaviour of the application.
+    var config = {
+      use24Hour: false, // Whether or not to use the 24 hour clock for the hour display.
+      dummyHour: 13, // Used to test the styling of the hour rows, use "null" to use the current time or an integer specifying the hour to use.
+      startHour: 8, // The first hour in the day to display.
+      endHour: 18, // The last hour in the day to display.
+      currentDateFormat: "dddd, Do MMMM YYYY" // The format of the date-time string in the jumbotron.
+    };
+
     return {
-      hours: { start: 8, end: 18 },
-      use24Hour: true,
-      dummyHour: 13,
-      currentDateFormat: "dddd, Do MMMM YYYY",
-
-      getHourFormat() { 
-        return this.use24Hour ? "H" : "ha";
-      },
-
-      getCurrentHour() {
-        return this.dummyHour ? this.dummyHour :  moment().hour();
-      }
+      hours: { start: config.startHour, end: config.endHour },
+      currentDateFormat: config.currentDateFormat,
+      hourFormat: config.use24Hour ? "H" : "ha",
+      currentHour: config.dummyHour ? config.dummyHour :  moment().hour()
     };
   }
 
@@ -26,7 +25,7 @@ $(() => {
     $("#currentDay").text(currentDay);
   }
 
-  // appends a new hour row to the "container" class.
+  // Appends a new hour row to the "container" class.
   function appendHourRow(hour, settings) {
     const row = $("<div>")
       .addClass("row")
@@ -34,10 +33,10 @@ $(() => {
       .append(
         $("<div>")
           .addClass("hour")
-          .text(moment(hour, "H").format(settings.getHourFormat()))
+          .text(moment(hour, "H").format(settings.hourFormat))
     );
 
-    const currentHour = settings.getCurrentHour();
+    const currentHour = settings.currentHour;
     
     row.append($("<textarea>")
       .addClass(
@@ -49,8 +48,11 @@ $(() => {
       .val(localStorage.getItem("hour"+ hour))
     );
     
-    row.append($("<button>").addClass("saveBtn").append(
-      $("<i>").addClass("fa-solid fa-floppy-disk")
+    row.append($("<button>")
+      .addClass("saveBtn")
+      .append(
+        $("<i>")
+          .addClass("fa-solid fa-floppy-disk")
     ));
 
     $(".container").append(row);
@@ -62,21 +64,25 @@ $(() => {
     const hour = button.parent().attr("data-hour");
     const text = button.siblings("textarea").val().trim();
     
-
     if (text === "") {
       localStorage.removeItem("hour" + hour);
     } else {
       localStorage.setItem("hour" + hour, text);
     }
   }
-  
-  const settings = getSettings();
 
-  setCurrentDay(moment().format(settings.currentDateFormat));
+  // Application initialisation.
+  function init() {
+    const settings = getSettings();
 
-  for (let hour = settings.hours.start; hour <= settings.hours.end; hour++) {
-    appendHourRow(hour, settings);
+    setCurrentDay(moment().format(settings.currentDateFormat));
+
+    for (let hour = settings.hours.start; hour <= settings.hours.end; hour++) {
+      appendHourRow(hour, settings);
+    }
+
+    $("div.container").on("click", "button", saveButtonClick)
   }
 
-  $("div.container").on("click", "button", saveButtonClick)
+  init();
 });
