691f131d45c05b8673bc02dead780cacd0ade002
Fezzer <chris.bowles@thescreamingsheep.net>
2023-01-22 00:05:10 +0000

    refactor script.js and tweak layout of the "hour" class

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,47 @@
 "use strict";
 
+// gets the settings for the application.
+function getSettings() {
+  return {
+    hours: { start: 8, end: 18 },
+    use24Hour: true,
+    dummyHour: 13,
+    currentDateFormat: "dddd, Do MMMM YYYY",
+
+    getHourDisplayFormat() { 
+      return this.use24Hour ? "H" : "ha";
+    },
+
+    getCurrentHour() {
+      return this.dummyHour ? this.dummyHour :  moment().hour();
+    }
+  };
+}
+
+// Sets the current day element in the UI to the specified value.
+function setCurrentDay(currentDay) {
+  $("#currentDay").text(currentDay);
+}
+
+// Application initialisation.
 $(() => {
-  const hours = { start: 8, end: 18 };
+  const settings = getSettings();
+  const currentHour = settings.getCurrentHour();
 
-  $(currentDay).text(moment().format("dddd, Do MMMM YYYY"));
+  setCurrentDay(moment().format(settings.currentDateFormat));
 
-  const currentHour = 13; // moment().hour();
-
-  for (let i = hours.start; i <= hours.end; i++) {
-    const row = $("<div>").addClass("row");
-    
-    row.append($("<div>").addClass("hour").text(i));
+  for (let i = settings.hours.start; i <= settings.hours.end; i++) {
+    const row = $("<div>").addClass("row").append(
+      $("<div>").addClass("hour").text(moment(i, "H").format(settings.getHourDisplayFormat()))
+    );
     
     row.append($("<textarea>").addClass(
       i < currentHour 
         ? "past" 
         : i > currentHour 
             ? "future" 
-            : "present"));
+            : "present"
+    ));
     
     row.append($("<button>").addClass("saveBtn").append(
       $("<i>").addClass("fa-solid fa-floppy-disk")
